34af5b40710dc0f264d45cc5f4712abcd1dc39b8
David Verhulst <david.verhulst@ap.be>
2022-10-28 15:13:29 +0200

    added deploy stuff and readline stuff

--- index.js
+++ index.js
@@ -1,37 +1,44 @@
-import fs from "fs"; // import nodejs core library
+const fs = require("fs"); // import nodejs core library
+const rl = require("readline-sync");
 
-const folderToRead = process.argv[2]; // grab third array argument
-
-if (!fs.existsSync(folderToRead)) {
-  // check if folder does not exist
-  console.error("Folder could not be found !!!");
-} else {
-  // folder exists...
-  const folderContents = fs.readdirSync(folderToRead);
-  let allContent = "";
-  for (let i = 0; i < folderContents.length; i++) {
-    allContent += fs.readFileSync(folderToRead + "/" + folderContents[i]);
+while (true) {
+  const folderToRead = rl.question(
+    "Please provide a path to your subtitle folder...\n(or enter 'STOP' to close) \n"
+  ); // grab third array argument
+  if (folderToRead === "STOP") {
+    process.exit();
   }
-  allContent = allContent
-    .replace(/[^a-z]/gi, " ")
-    .replace(/font|color/gi, " ")
-    .replace(/ +/g, " ")
-    .toLowerCase()
-    .split(" ")
-    .filter(function (word) {
-      return word.length >= 4;
+  if (!fs.existsSync(folderToRead)) {
+    // check if folder does not exist
+    console.error("Folder could not be found !!!");
+  } else {
+    // folder exists...
+    const folderContents = fs.readdirSync(folderToRead);
+    let allContent = "";
+    for (let i = 0; i < folderContents.length; i++) {
+      allContent += fs.readFileSync(folderToRead + "/" + folderContents[i]);
+    }
+    allContent = allContent
+      .replace(/[^a-z]/gi, " ")
+      .replace(/font|color/gi, " ")
+      .replace(/ +/g, " ")
+      .toLowerCase()
+      .split(" ")
+      .filter(function (word) {
+        return word.length >= 4;
+      });
+
+    const wordsWithOccurrence = allContent.reduce(function (obj, word) {
+      obj[word] = obj[word] + 1 || 1;
+      return obj;
+    }, {});
+
+    const uniqueWords = Object.keys(wordsWithOccurrence);
+
+    uniqueWords.sort(function (wordA, wordB) {
+      return wordsWithOccurrence[wordB] - wordsWithOccurrence[wordA];
     });
 
-  const wordsWithOccurrence = allContent.reduce(function (obj, word) {
-    obj[word] = obj[word] + 1 || 1;
-    return obj;
-  }, {});
-
-  const uniqueWords = Object.keys(wordsWithOccurrence);
-
-  uniqueWords.sort(function (wordA, wordB) {
-    return wordsWithOccurrence[wordB] - wordsWithOccurrence[wordA];
-  });
-
-  console.log(uniqueWords);
+    console.log(uniqueWords);
+  }
 }
